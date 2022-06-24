2de00160cd8415f3fcaf4b1bf2bb3a704abe5bb8
ihascats <s.pavlovic.rs@gmail.com>
2022-06-24 19:19:49 +0200

    Try Hashrouter basename instead

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import App from '../App';
 import Cart from '../Cart';
 import Shop from '../Shop';
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename="https://ihascats.github.io/shopping-cart">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+    <BrowserRouter>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </HashRouter>
     </BrowserRouter>
   );
 };

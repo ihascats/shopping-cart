ae2b1640281102e99d49da9eba0300124022f5e6
ihascats <s.pavlovic.rs@gmail.com>
2022-06-25 14:45:43 +0200

    Change to hashRouter for routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import App from '../App';
 import Cart from '../Cart';
 import Shop from '../Shop';
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <Routes>
         <Route path={process.env.PUBLIC_URL + '/'} element={<App />} />
         <Route path={process.env.PUBLIC_URL + '/shop'} element={<Shop />} />
         <Route path={process.env.PUBLIC_URL + '/cart'} element={<Cart />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 

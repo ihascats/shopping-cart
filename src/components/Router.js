d796e9b7b8e79dd7fd4edd42b8ba69307766a5dc
ihascats <s.pavlovic.rs@gmail.com>
2022-06-25 10:09:02 +0200

    Revert changes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App';
 import Cart from '../Cart';
 import Shop from '../Shop';
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
-      <HashRouter basename="/">
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </HashRouter>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </BrowserRouter>
   );
 };
